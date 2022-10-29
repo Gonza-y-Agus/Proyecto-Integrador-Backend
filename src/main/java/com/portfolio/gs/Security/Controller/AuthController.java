2f0f43904d0446e38166fd515eaaf854feddce31
Gonza-y-Agus <gonzalo.smeriglio@gmail.com>
2022-10-29 06:13:25 -0300

    Corrigiendo CORS en el backend

--- src/main/java/com/portfolio/gs/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/gs/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = {"http://localhost:4200", "https://frotendgs1.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
